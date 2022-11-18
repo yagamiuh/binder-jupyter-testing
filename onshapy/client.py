0eb7fd8c8b7509eba520bcffcc8887bf75d595e4
Yanoel Llano Boitel <yllano@merthin.com>
2022-11-18 16:57:29 +0100

    Testing working

--- onshapy/client.py
+++ onshapy/client.py
@@ -1,21 +1,35 @@
-import requests, json
+import requests, json, os
 
 class OnShapeClient:
     def __init__(self, client = ""):
+        print("working...")
         self.client = client
         self.__refresh_token()
 
     def __refresh_token(self):
-        try:
-            url = "https://onshape-jupyter-extension.cyclic.app/api/get-token"
-            payload= f'hash={self.client}'
-            headers = { 'Content-Type': 'application/x-www-form-urlencoded' }
-            response = requests.request("POST", url, headers = headers, data = payload)
-            response = json.loads(response.text)
-            self.access_token = response["access_token"]
-            return True
-        except:
-            return False
+        if self.client != "":
+            try:
+                url = "https://onshape-jupyter-extension.cyclic.app/api/get-token"
+                payload= f'hash={self.client}'
+                headers = { 'Content-Type': 'application/x-www-form-urlencoded' }
+                response = requests.request("POST", url, headers = headers, data = payload)
+                response = json.loads(response.text)
+                self.access_token = response["access_token"]
+                return True
+            except:
+                return False
+        else:
+            pass
+            # try:
+            #     url = "https://onshape-jupyter-extension.cyclic.app/api/get-token-jupyterhub"
+            #     payload= f'user={os.environ["JUPYTERHUB_USER"]};token={os.environ["JUPYTERHUB_API_TOKEN"]}'
+            #     headers = { 'Content-Type': 'application/x-www-form-urlencoded' }
+            #     response = requests.request("POST", url, headers = headers, data = payload)
+            #     response = json.loads(response.text)
+            #     self.access_token = response["access_token"]
+            #     return True
+            # except:
+            #     return False
 
     def request(self, method, endpoint, data = None):
         url = f"https://cad.onshape.com/api/v5/{endpoint}"
